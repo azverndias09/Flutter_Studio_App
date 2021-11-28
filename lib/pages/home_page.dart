e723da27676ca2c88643312a0f0a5694f62ed84b
azverndias09 <azverndias09@gmail.com>
2021-11-28 11:54:55 +0530

    list view of products

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:gram_clone/models/catalog.dart';
+import 'package:gram_clone/pages/widgets/item_widget.dart';
+
 import 'package:gram_clone/pages/widgets/widgets.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "AzvernGram",
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("welcome to flutter"),
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          }),
       drawer: MyDrawer(),
     );
   }
